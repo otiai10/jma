cfab2aa079a26065ce5482b0ee1bcff87f35007c
Hiromu OCHIAI <otiai10@gmail.com>
2021-07-12 12:51:53 +0900

    Add small tests

---
+++ forecast/all_test.go
@@ -0,0 +1,33 @@
+package forecast
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/otiai10/jma"
+	"github.com/otiai10/marmoset"
+	. "github.com/otiai10/mint"
+)
+
+func TestForecastByCity(t *testing.T) {
+	r := marmoset.NewRouter()
+	r.GET("/data/forecast/130000.json", func(w http.ResponseWriter, r *http.Request) {
+		if f, err := os.Open("example_130000.json"); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		} else {
+			io.Copy(w, f)
+		}
+	})
+	s := httptest.NewServer(r)
+	defer s.Close()
+
+	c := NewClient()
+	c.BaseURL = s.URL
+
+	res, err := c.ForecastByCity(jma.Tokyo)
+	Expect(t, err).ToBe(nil)
+	Expect(t, len(res)).ToBe(2)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/otiai10/jma
 
 go 1.15
+
+require (
+	github.com/otiai10/marmoset v0.5.0
+	github.com/otiai10/mint v1.3.2
+)
